9cdb527e702085e8f0747c498b032eda3ca19ec6
JW <artoriamaster@gmail.com>
2023-08-18 14:54:39 -0400

    Initial commit

---
+++ src/Counter.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockToken is ERC20 {
+    constructor() ERC20("MockToken", "MTK") {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
